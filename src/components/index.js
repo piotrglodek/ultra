1c28ed921a3d0d4a203883ac8c07937da69d5605
Piotr Glodek <glodekpiotr@wp.pl>
2020-09-18 14:58:28 +0200

    signIn and Up

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { default as Nav } from './Nav/Nav';
 export { default as Section } from './Section/Section';
 export { default as Footer } from './Footer/Footer';
 export { default as Card } from './Card/Card';
+export { default as SignIn } from './SignIn/SignIn';
+export { default as SignUp } from './SignUp/SignUp';
