6d618f6ce8ade583b6f7f7e43f26c294306a0476
Piotr Glodek <glodekpiotr@wp.pl>
2020-09-18 14:40:17 +0200

    home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Providers from './Providers';
+// components
 import { Nav } from './components';
+// pages
+import { Home } from './pages';
 
 function App() {
   return (
     <Providers>
       <Nav />
+      <Switch>
+        <Route path='/' exact component={Home} />
+      </Switch>
     </Providers>
   );
 }
