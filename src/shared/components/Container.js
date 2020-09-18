a20327365091e465f65e9bf4f7fefc11bcc2ab9e
Piotr Glodek <glodekpiotr@wp.pl>
2020-09-18 14:31:07 +0200

    container component

---
+++ src/shared/components/Container.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 0 5rem;
+  @media screen and (max-width: 991px) {
+    padding: 0 2rem;
+  }
+`;
